b054d8a59b0e7a1dc993d4ea5fe4edc326f0c306
mino29 <42552577+mino29@users.noreply.github.com>
2022-08-18 18:15:10 +0800

    test

--- upload_local_nvimconf.ps1
+++ upload_local_nvimconf.ps1
@@ -2,6 +2,8 @@
 # find the default neovim init.vim file and copy here
 # find the whole .vim_runtime folder, and copy here (overwrite if necessary)
 
-Copy-Item ~/AppData/Local/nvim/init.vim ~/Documents/GitHub/neovim_config/init.vim
-Copy-Item -Recurse -Force ~/.vim_runtime ~/Documents/GitHub/neovim_config/
+Copy-Item -Recurse -Force ~/AppData/Local/nvim ~/Documents/GitHub/neovim_config/
 
+Write-Host "You've upload your local neovim config, ready to commit"
+
+git add .
