280a3f8e7188a84bd16358a050df5d93584eabfa
mino29 <42552577+mino29@users.noreply.github.com>
2022-09-04 16:03:59 +0800

    update alpha, config file now default to init.lua path regardless of your os

--- install.ps1
+++ install.ps1
@@ -1,15 +1,10 @@
 # Clone the git to the .vim directory of home
-git clone https://github.com/mino29/neovim_config.git $home\AppData\Local\nvim
+git clone https://github.com/mino29/meovim.git "$env:LOCALAPPDATA\nvim"
 
-
-# Install vim-plug package manager
-echo "Setting up vim-plug manager for neovim"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # install plugins in vim with Vundle
 echo "Installing vim plugins"
-nvim +PlugInstall +qa!
+nvim +PackerSync +qa!
 
 # Give feedback that we installed the ultimate vimrc
-echo "Installed the Ultimate Vim setup."
+echo "Meovim Installed, meow~"
--- install.sh
+++ install.sh
@@ -5,23 +5,7 @@ host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
 export ALL_PROXY="http://$host_ip:7890"
 
 # Clone the git to the .vim directory of home
-git clone https://github.com/mino29/neovim_config.git ~/.config/nvim
-
-# Trenger jeg denne?
-# cd ~/.config/nvim
-
-# Import the vimrc files
-# echo '" Import vim files
-# source ~/.vim/runtime/vimrcs/import.vim
-# ' > ~/.config/nvim/init.vim
-
-# Create vimundo file for making "infinite" undoes for all files
-# mkdir ~/.vim/vimundo
-
-# Install vim-plug package manager
-echo "Setting up vim plugin manager for neovim"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+git clone https://github.com/mino29/meovim.git ~/.config/nvim
 
 # Brows like a pro with ctags
 sudo apt install universal-ctags
@@ -30,11 +14,11 @@ sudo apt install universal-ctags
 cp -r nvim ~/.config/
 
 # install plugins in vim with vim plug
-echo "Installing vim plugins"
-nvim +PlugInstall +qa!
+echo "Installing plugins"
+nvim +PackerSync +qa!
 
 # install coc.nvim
 nvim +CocUpdate +qa!
 
 # Give feedback that we installed the ultimate vimrc
-echo "Installed the Ultimate Vim setup."
+echo "Meovim insatlled, meow~"
