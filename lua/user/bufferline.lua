9cb4f98e596f2f1b7ade29e7b9011404b5aa8d88
mino29 <42552577+mino29@users.noreply.github.com>
2022-10-15 14:40:38 +0800

    hotfix, colorscheme and bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -4,10 +4,10 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
-    always_show_bufferline = false,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
+    -- separator_style = 'slant',
+    always_show_bufferline = true,
+    show_buffer_close_icons = true,
+    show_close_icon = true,
     color_icons = true
   },
   highlights = {
