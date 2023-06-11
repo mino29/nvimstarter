876af20aca63734d4ea8994d6c5720bf4eeaa33c
mino29 <42552577+mino29@users.noreply.github.com>
2023-06-12 00:28:48 +0800

    remove unused themes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -76,7 +76,6 @@ return packer.startup(function(use)
 
 	-- Colorschemes
     use {'dracula/vim', as = 'dracula' }-- Dracula theme
-    use ('shaunsingh/nord.nvim')
     use ('folke/tokyonight.nvim')
 
 	-- cmp plugins
