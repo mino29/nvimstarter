bfd48a06f89c16284f5718f952a61e72303531f9
mino29 <42552577+mino29@users.noreply.github.com>
2023-11-13 00:42:02 +0800

    add my own keybinds from meovim

--- init.lua
+++ init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("custom.keymaps")
+require("custom.options")
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+local is_mac = has("macunix")
+local is_linux = has("linux")
+local is_win = has("win32")
+
+if is_mac then
+  require("custom.macos")
+end
+if is_linux then
+  require("custom.linux")
+end
+if is_win then
+  require("custom.windows")
+end
